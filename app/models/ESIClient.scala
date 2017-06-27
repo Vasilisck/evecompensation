eee3b2d55b6ee6fc38a804427fd91593de838185
Aleksandr Baranov <aleksandr.baranov@accenture.com>
2017-06-27 17:13:43 +0300

    get killmail throught eve ESI api

--- app/models/ESIClient.scala
+++ app/models/ESIClient.scala
@@ -2,6 +2,10 @@ package models
 
 import javax.inject.Inject
 
+import play.api.libs.json.{Json, Reads}
+import play.api.libs.ws.{WSClient, WSRequest}
+
+import scala.concurrent.{ExecutionContext, Future}
 import scala.io.Source
 
 /**
@@ -9,12 +13,40 @@ import scala.io.Source
   */
 
 
-class ESIClient {
+class ESIClient @Inject()(ws: WSClient)(implicit context: ExecutionContext) {
 
+
+  //Implicit readers for Json to case class
+  private implicit val attackerReads: Reads[Attacker] = Json.reads[Attacker]
+  private implicit val itemReads: Reads[Item] = Json.reads[Item]
+  private implicit val positionReads: Reads[Position] = Json.reads[Position]
+  private implicit val victimReads: Reads[Victim] = Json.reads[Victim]
+  private implicit val killmailReads: Reads[Killmail] = Json.reads[Killmail]
+
+  //start init
   private val buffer = Source.fromFile("ESISettings")
   private val source = buffer.getLines()
+
   val clientID: String = source.next()
   val securityKey: String = source.next()
-  buffer.close()
+  private val killmailUrl: String = "https://esi.tech.ccp.is/latest/killmails"
 
+  buffer.close()
+  //end init
+
+  def getKillmailByIdAndHash(killmailId: String, killmailHash: String): Future[Killmail] = {
+    val request: WSRequest = ws
+      .url(s"$killmailUrl/$killmailId/$killmailHash")
+      .withQueryStringParameters(("datasource", "tranquility"))
+      .withHttpHeaders(("Accept", "application/json"))
+    val result = request.get().map(response => {
+      if(response.status == 200) {
+        val json = response.json
+        json.as[Killmail]
+      } else {
+        throw new Exception(s"code ${response.status}, ${response.body}")
+      }
+    })
+    result
+  }
 }
--- test/models/ESIClientSpec.scala
+++ test/models/ESIClientSpec.scala
@@ -1,18 +1,30 @@
 package models
 
 import org.scalatestplus.play.PlaySpec
+import org.scalatestplus.play.guice.GuiceOneAppPerTest
+import play.api.test._
+
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
 
 /**
   * Created by vasilisck on 6/26/17.
   */
-class ESIClientSpec extends PlaySpec {
+class ESIClientSpec extends PlaySpec with GuiceOneAppPerTest with Injecting {
 
   "ESI Client" should {
 
-    "read key properly" ignore {
-      val eSIClient = new ESIClient()
+
+    "read client id and security key properly" ignore {
+      val eSIClient = inject[ESIClient]
       assertResult("client id here")(eSIClient.clientID)
       assertResult("security key here")(eSIClient.securityKey)
+    }
+
+    "get killmails" in {
+      val eSIClient = inject[ESIClient]
+      val result = Await.result(eSIClient.getKillmailByIdAndHash("63042503", "952d9b640424d10a2d2ef4ca57588ea6434c9a3c"), Duration.Inf)
+      assertResult(38)(result.attackers.size)
     }
   }
 
